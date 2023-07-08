d5f5fb95609dec0c2c3fe9fd8b50c07c4e01d8a1
Sam Redmond <sam@fingermarkglobal.com>
2023-07-08 23:49:33 +1200

    refactor

--- backend/app.ts
+++ backend/app.ts
@@ -5,7 +5,6 @@ import { Server as IOServer, Socket } from 'socket.io';
 import { Player, Game, Round, Move, MoveType, TurnState } from './lib/state';
 
 import logger from './lib/logger';
-import assert from 'assert';
 
 config();
 
@@ -13,70 +12,105 @@ const app: Application = express();
 const server = new Server(app);
 const io = new IOServer(server);
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 const game = new Game();
-let round: Round | null | undefined;
+let round: Round | null;
 
+/**
+ * Socket IO Events
+ */
+const EVENT_TYPES = {
+  REGISTER: 'register',
+  START: 'start',
+  ACTION: 'action',
+  POT_UPDATED: 'pot_updated',
+  USER_UPDATED: 'user_updated',
+  TURN_TAKEN: 'turn_taken',
+}
+
+/**
+ * Socket IO Handler
+ */
 io.on('connection', (socket: Socket) => {
+  // register event
+  socket.on(EVENT_TYPES.REGISTER, (name: string, chips: number) => registerPlayer(name, chips, socket));
 
-	/**
-	 * Register player into game
-	 */
-	socket.on('register', (name: string, chips: number) => {
-		logger.info(`Player ${name} registered with ${chips} chips`);
-		const player = new Player(socket.id, name, chips);
-		game.addPlayer(player);
-	});
+  // start game event
+  socket.on(EVENT_TYPES.START, () => startGame());
 
-	/**
-	 * Start the game
-	 */
-	socket.on('start', () => {
-		logger.info(`The game has now started at ${Date.now()}`)
-		round = game.nextRound();
-		io.emit('new_round', { round });
-	});
-
-	/**
-	 * Player to perform action
-	 */
-	socket.on('action', (action: string, amount?: number) => {
-		assert(round);
-
-		const player = game.getPlayerById(socket.id);
-		assert(player);
-
-		logger.info(`Player ${player.name} performed action ${action}`);
-
-		let turnTaken;
-
-		switch (action) {
-			case 'fold':
-				turnTaken = round.playMove(new Move(MoveType.Fold));
-				break;
-			case 'call':
-				turnTaken = round.playMove(new Move(MoveType.Call));
-				break;
-			case 'raise':
-				turnTaken = round.playMove(new Move(MoveType.Raise, amount));
-				break;
-			default:
-				logger.error('Player has performed an invalid action');
-				return;
-		}
-
-		const pot = round.currentPot;
-
-		io.emit('pot_updated', pot);
-		io.emit('user_updated', player);
-		io.emit('turn_taken', { round, turnTaken });
-	});
+  // action event
+  socket.on(EVENT_TYPES.ACTION, (action: string, amount: number) => performAction(action, amount, socket));
 });
 
 /**
- * Start the web server
+ * Register the player into the game
  */
-app.listen(PORT, () => {
-	logger.info(`Server is listening on port ${PORT}`);
-});
+const registerPlayer = (name: string, chips: number, socket: Socket) => {
+  logger.info(`Player ${name} registered with ${chips} chips`);
+  const player = new Player(socket.id, name, chips);
+  game.addPlayer(player);
+
+  io.emit(EVENT_TYPES.USER_UPDATED, player);
+}
+
+/**
+ * Initiate the game
+ */
+const startGame = () => {
+  logger.info(`The game has now started at ${Date.now()}`)
+  round = game.nextRound();
+
+  io.emit(EVENT_TYPES.START, { round });
+}
+
+/**
+ * Player to perform an action
+ */
+const performAction = (action: string, amount: number, socket: Socket) => {
+  if (!round) {
+    logger.error('Player tried to perform an action before the round begun');
+    return;
+  }
+
+  const player = game.getPlayerById(socket.id);
+  
+  if (!player) {
+    logger.error('Someone tried to perform an action before registering');
+    return;
+  }
+
+  if (round.currentPlayer() != player) {
+    logger.error('Player tried to perform an action when it was not their turn');
+  }
+
+  logger.info(`Player ${player.name} performed action ${action}`);
+
+  let turnTaken: TurnState;
+
+  switch (action) {
+    case 'fold':
+      turnTaken = round.playMove(new Move(MoveType.Fold));
+      break;
+    case 'call':
+      turnTaken = round.playMove(new Move(MoveType.Call));
+      break;
+    case 'raise':
+      turnTaken = round.playMove(new Move(MoveType.Raise, amount));
+      break;
+    default:
+      logger.error('Player has performed an invalid action');
+      return;
+  }
+
+  const pot = round.currentPot;
+
+  io.emit('pot_updated', pot);
+  io.emit('user_updated', player);
+  io.emit('turn_taken', { round, turnTaken });
+}
+
+/**
+ * Start the IO server
+ */
+io.listen(PORT);
