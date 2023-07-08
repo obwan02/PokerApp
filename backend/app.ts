17804ad9c668b925df373ecaf6d560609423f229
Sam Redmond <sam@fingermarkglobal.com>
2023-07-08 22:16:37 +1200

    moved logic into main app

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,7 @@
 import { config } from 'dotenv';
 import express, { Application } from 'express';
 import { Server } from 'http';
-import { Server as IOServer } from 'socket.io';
-import { socketHandler } from './lib/socket';
+import { Server as IOServer, Socket } from 'socket.io';
 
 config();
 
@@ -12,7 +11,49 @@ const io = new IOServer(server);
 
 const PORT = process.env.PORT || 3000;
 
-socketHandler(io);
+const game = new Game();
+let round: Round;
+
+io.on('connection', (socket: Socket) => {
+
+	socket.on('register', (name: string, chips: number) => {
+	  const player = new Player(socket.id, name, chips);
+	  game.addPlayer(player);
+	});
+
+	socket.on('start', () => {
+		round = game.startRound();
+		io.emit('started');
+	});
+  
+	socket.on('action', (action: string, amount: number = 0) => {
+		const player = game.getPlayerById(socket.id);
+
+		if (!player) {
+			throw new Error('Player could not be found');
+		}
+
+	  switch (action) {
+			case 'fold':
+				//player.status = STATUS.folded;
+		  	break;
+			case 'call':
+				//player.status = STATUS.playing;
+				break;
+			case 'raise':
+				//player.status = STATUS.playing;
+				//player.chips -= amount;
+				//round.addToPot(amount);
+				break;
+	  }
+
+	  const pot = round.currentPot;
+
+	  io.emit('pot_updated', pot);
+	  io.emit('user_updated', player);
+	});
+  });
+  
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
