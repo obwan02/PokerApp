255a1c794f208c27956ed54979f2f1a2a174de43
Jonty Morris <jonty.morris@yahoo.co.nz>
2023-07-09 02:05:51 +1200

    Real data..."

--- frontend/src/app/player/player.component.ts
+++ frontend/src/app/player/player.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Player } from '../models/state';
 
 @Component({
   selector: 'app-player',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerComponent implements OnInit {
 
+  @Input() player?: Player;
+  
   constructor() { }
 
   ngOnInit(): void {
