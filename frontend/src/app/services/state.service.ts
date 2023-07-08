52f2918d6f701af8871ee9e10eca696503b982f8
obwan02 <obwan02@hotmail.co.nz>
2023-07-09 00:22:32 +1200

    update: Most of state service completed

--- frontend/src/app/services/state.service.ts
+++ frontend/src/app/services/state.service.ts
@@ -1,20 +1,41 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { io, Socket } from 'socket.io-client';
+import { TurnState, Player, Round } from 'src/app/models/state'
 
 @Injectable({
   providedIn: 'root'
 })
 export class StateService {
+  
+
+  static EVENT_TYPES = {
+    REGISTER: 'register', // send
+    START: 'start', // send and recieve
+    ACTION: 'action', // send
+    TURN_TAKEN: 'turn_taken', // recieve
+    NEW_PLAYER: 'new_player' // recieve
+  };
 
   socket?: Socket;
 
-  constructor() {
+  playerJoined: EventEmitter<Player> = new EventEmitter();
+  roundStarted: EventEmitter<Round> = new EventEmitter();
+  turnTaken: EventEmitter<{turnTaken: TurnState, round: Round}> = new EventEmitter();
 
+  constructor() {}
+
+  private registerEvents() {
+    this.socket?.on(StateService.EVENT_TYPES.NEW_PLAYER, this.playerJoined.emit);
+    this.socket?.on(StateService.EVENT_TYPES.START, this.roundStarted.emit);
+    this.socket?.on(StateService.EVENT_TYPES.TURN_TAKEN, this.turnTaken.emit);
   }
 
   join(host: string, name: string, chips: number): Observable<void> {
     this.socket = io(host);
+
+    // register events
+    this.registerEvents();
 
     return new Observable(observer => {
       
@@ -23,16 +44,12 @@ export class StateService {
         return;
       }
 
-      this.socket.emit('register', {
+      this.socket.emit(StateService.EVENT_TYPES.REGISTER, {
         name,
         chips
       }, (_: any) => {
         observer.complete();
       });
     });
-    
   }
-
-
-
 }
